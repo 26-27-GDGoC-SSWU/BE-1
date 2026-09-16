6fa2839329e3debc1b88d5861007e14e6581c83c
jsm52277 <jsm52277@gmail.com>
2026-09-16 12:46:17 +0900

    수민 week01 실습 파일 제출

--- week01/SM_06_select.sql
+++ week01/SM_06_select.sql
@@ -1,17 +1,26 @@
 USE market_db;
+SELECT * 
+FROM member;
 
-SELECT * FROM member;
-
-SELECT * FROM member WHERE mem_name = '소녀시대';
+SELECT * 
+FROM member WHERE mem_name = '소녀시대';
 SELECT mem_name 그룹명, debut_date '활동 시작일', mem_number '멤버 수' FROM member;
 
-SELECT mem_name, height FROM member WHERE height >= 165 AND height <= 167;
-SELECT mem_name, height FROM member WHERE height BETWEEN 165 AND 167;
+SELECT mem_name, height
+FROM member WHERE height >= 165 AND height <= 167;
+SELECT mem_name, height
+FROM member WHERE height BETWEEN 165 AND 167;
 
-SELECT mem_name, addr FROM member WHERE addr = '서울' OR addr = '경북';
-SELECT mem_name, addr FROM member WHERE addr IN('서울', '경북');
+SELECT mem_name, addr
+FROM member WHERE addr = '서울' OR addr = '경북';
+SELECT mem_name, addr
+FROM member WHERE addr IN('서울', '경북');
 
-SELECT mem_name FROM member WHERE mem_name LIKE '마%';
-SELECT mem_name FROM member WHERE mem_name LIKE '%걸';
-SELECT mem_name FROM member WHERE mem_name LIKE '___시대';
-SELECT mem_name FROM member WHERE mem_name LIKE '에이__';
+SELECT mem_name
+FROM member WHERE mem_name LIKE '마%';
+SELECT mem_name
+FROM member WHERE mem_name LIKE '%걸';
+SELECT mem_name 
+FROM member WHERE mem_name LIKE '__시대';
+SELECT mem_name
+FROM member WHERE mem_name LIKE '에이__';
