c68f094b2cf10e14b5e669b0f060f8045d2dadb9
jsm52277 <jsm52277@gmail.com>
2026-09-16 12:40:25 +0900

    Update SM_06_select.sql

--- week01/SM_06_select.sql
+++ week01/SM_06_select.sql
@@ -1,15 +1,17 @@
 USE market_db;
 
-SELECT * FROM market_db.member WHERE mem_name = '블랙핑크';
-SELECT addr, height 키 , debut_date "데뷔 일자" FROM member;
-SELECT * FROM member WHERE mem_name = '블랙핑크';
-SELECT * FROM member WHERE mem_number = 4;
-SELECT mem_id, mem_name FROM member WHERE height <= 162;
-SELECT mem_name, height, mem_number FROM member WHERE height >= 165 AND mem_number > 6;
-SELECT mem_name, height, mem_number FROM member WHERE height >= 165 OR mem_number > 6;
-SELECT mem_name, height FROM member WHERE height >= 163 AND height <= 165;
-SELECT mem_name, height FROM member WHERE height BETWEEN 163 AND 165;
-SELECT mem_name, addr FROM member WHERE addr = '경기' OR addr = '전남' OR addr = '경남';
-SELECT mem_name, addr FROM member WHERE addr IN('경기', '전남', '경남');
-SELECT * FROM member WHERE mem_name LIKE '우%';
-SELECT * FROM member WHERE mem_name LIKE '__핑크';
+SELECT * FROM member;
+
+SELECT * FROM member WHERE mem_name = '소녀시대';
+SELECT mem_name 그룹명, debut_date '활동 시작일', mem_number '멤버 수' FROM member;
+
+SELECT mem_name, height FROM member WHERE height >= 165 AND height <= 167;
+SELECT mem_name, height FROM member WHERE height BETWEEN 165 AND 167;
+
+SELECT mem_name, addr FROM member WHERE addr = '서울' OR addr = '경북';
+SELECT mem_name, addr FROM member WHERE addr IN('서울', '경북');
+
+SELECT mem_name FROM member WHERE mem_name LIKE '마%';
+SELECT mem_name FROM member WHERE mem_name LIKE '%걸';
+SELECT mem_name FROM member WHERE mem_name LIKE '___시대';
+SELECT mem_name FROM member WHERE mem_name LIKE '에이__';
