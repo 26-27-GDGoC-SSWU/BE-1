bb28a6c91ec930686d477780acafb149e6f25705
jsm52277 <jsm52277@gmail.com>
2026-09-16 16:58:32 +0900

    수민 week01 실습 파일 제출

--- week01/SM_09_data_type.sql
+++ week01/SM_09_data_type.sql
@@ -1,13 +1,29 @@
 USE market_db;
-USE netflix_db;
+
+CREATE TABLE hongong4 (
+    tinyint_col TINYINT,
+    smallint_col SMALLINT,
+    int_col INT,
+    bigint_col BIGINT
+);
 
 INSERT INTO hongong4 VALUES(127, 32767, 2147483647, 9000000000000000000);
 
-SET @myVar1 = 5;
-SET @myVar2 = 4.25;
+CREATE TABLE big_table (
+    data2 VARCHAR(16383)
+);
 
-SELECT @myVar1;
-SELECT @myVar1 + @myVar2;
+CREATE DATABASE IF NOT EXISTS netflix_db;
+USE netflix_db;
+
+CREATE TABLE movie (
+    movie_id INT,
+    movie_title VARCHAR(30),
+    movie_script LONGTEXT,
+    movie_film LONGBLOB
+);
+
+USE market_db;
 
 SET @txt = '가수 이름==> ';
 SET @height = 166;
@@ -16,37 +32,9 @@ SELECT @txt, mem_name
 FROM member 
 WHERE height > @height;
 
-SELECT '가수 이름==> ', mem_name 
-FROM member 
-WHERE height > 166;
-
-SELECT mem_name, height 
-FROM member 
-ORDER BY height 
-LIMIT 3;
-
 SET @count = 3;
 PREPARE mySQL FROM 'SELECT mem_name, height FROM member ORDER BY height LIMIT ?';
 EXECUTE mySQL USING @count;
 
-SELECT AVG(price) '평균 가격' 
-FROM buy;
-
 SELECT CAST(AVG(price) AS SIGNED) '평균 가격' 
 FROM buy;
-
-SELECT CONVERT(AVG(price), SIGNED) '평균 가격' 
-FROM buy;
-
-SELECT CAST('2022$12$12' AS DATE);
-SELECT CAST('2022/12/12' AS DATE);
-SELECT CAST('2022@12@12' AS DATE);
-
-SELECT num, 
-       CONCAT(CAST(price AS CHAR), 'X', CAST(amount AS CHAR), '=') '가격X수량', 
-       price * amount '구매액' 
-FROM buy;
-
-SELECT '100' + '200';
-SELECT CONCAT('100', '200');
-SELECT CONCAT(100, '200');
